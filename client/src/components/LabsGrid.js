5c6d346570ec3d80e53228ce298e1896efe3c923
Hello4545 <sbp3948@gmail.com>
2024-05-25 17:07:50 +0000

    labview 진행 전

--- client/src/components/LabsGrid.js
+++ client/src/components/LabsGrid.js
@@ -1,14 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import "./LabsGrid.css";
 
 const LabsGrid = () => {
     const [labs, setLabs] = useState([]);
+    const [isProfessor, setIsProfessor] = useState(false);
+    const navigate = useNavigate();
+
+    const checkLogin = async () => {
+        try {
+            const { data } = await axios.get("https://api.ellen.newbie.sparcsandbox.com/check-login", { withCredentials: true });
+            if (data.isLoggedIn && data.user && data.user.isProfessor) {
+                setIsProfessor(true);
+            }
+        } catch (error) {
+            console.error('Error checking login status:', error);
+        }
+    };
 
     useEffect(() => {
         const fetchLabs = async () => {
             try {
-                const { data } = await axios.get("https://api.ellen.newbie.sparcsandbox.com/labs",{withCredentials: true});
+                const { data } = await axios.get("https://api.ellen.newbie.sparcsandbox.com/labs", { withCredentials: true });
                 setLabs(data);
                 console.log(data);
             } catch (error) {
@@ -16,13 +30,22 @@ const LabsGrid = () => {
             }
         };
 
+        checkLogin();
         fetchLabs();
     }, []);
+
+    const handleCardClick = (labId) => {
+        navigate(`/LabView`);
+    };
+
+    const handleAddLab = () => {
+        console.log('Add Lab button clicked');
+    };
 
     return (
         <div className="labs-grid-container">
             {labs.map(lab => (
-                <div key={lab.id} className="lab-card">
+                <div key={lab.lab_id} className="lab-card" onClick={() => handleCardClick(lab.lab_id)}>
                     <h2 className="lab-title">{lab.lab_name}</h2>
                     <div className="lab-field">{lab.field}</div>
                     {/* <div className="lab-detail">Location: {lab.location}</div> */}
@@ -30,6 +53,9 @@ const LabsGrid = () => {
                     {/* <div className="lab-detail">Professor: {lab.professor.name}</div> */}
                 </div>
             ))}
+            {isProfessor && (
+                <button onClick={handleAddLab} className="add-lab-button">Add Lab</button>
+            )}
         </div>
     );
 };
