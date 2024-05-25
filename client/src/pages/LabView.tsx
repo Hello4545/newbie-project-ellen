7fae761a739d64fdc725ae930727da95d4b29048
Hello4545 <sbp3948@gmail.com>
2024-05-25 19:46:56 +0000

    add css

--- client/src/pages/LabView.tsx
+++ client/src/pages/LabView.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate, useLocation }  from "react-router-dom";
+import "./css/LabView.css";
 // import "./css/Home.css";
 
 axios.defaults.withCredentials = true;
@@ -12,6 +13,12 @@ const LabViewPage = (props: {}) => {
     const [website, setWebsite] = useState("");
     const [field, setField] = useState("");
 
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [isProfessor, setIsProfessor] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
     const location = useLocation();
     const labInfo = { ...location.state };
     console.log(labInfo.labId)
@@ -19,6 +26,25 @@ const LabViewPage = (props: {}) => {
     if(!location.state){
         console.log("error");
     }
+
+    const checkLogin = async () => {
+        try {
+          const {data} = await axios.get<{id: number; email: string; isProfessor: boolean; name: string}>("https://api.ellen.newbie.sparcsandbox.com/check-login", {withCredentials: true});
+          console.log(data);
+          if (data.id) {
+            setName(data.name);
+            setEmail(data.email);
+            setIsProfessor(data.isProfessor);
+            setIsLoggedIn(true);
+          } else {
+            setIsLoggedIn(false);
+          }
+          // console.log("Response : "+data.id);
+          // setIsLoggedIn(response.data.isProfessor);
+        } catch (error) {
+          console.error('Failed to fetch username:', error);
+        }
+      };
 
     const getLabInfo = async () => {
         try {
@@ -37,21 +63,47 @@ const LabViewPage = (props: {}) => {
         }
     };
     
+    const handleLogout = async () => {
+        try {
+          const response = await axios.post("https://api.ellen.newbie.sparcsandbox.com/logout", {withCredentials: true});
+          alert('Successfully logged out!');
+          navigate("/");
+        window.location.reload();
+        } catch (error) {
+          console.error('Failed to logout:', error);
+        }
+      }
+    
     useEffect(() => {
         getLabInfo();
+        checkLogin();
     }, []);
 
     return (
-        <div className="lab-view">
-            <h1>Lab Details</h1>
-            <div>
-                <div><label>Name:</label> {labname}</div>
-                <div><label>Location:</label> {labLocation}</div>
-                <div><label>Website:</label> {website} </div>
-                <div><label>Field of Study:</label> {field}</div>
+        <div>
+            <nav className="top-nav">
+                <div className="nav-title"><h1>Labs</h1></div>
+                <div className="nav-message">{isLoggedIn ? `Hello, ${name}!` : 'Please log in.'}</div>
+                <div className="nav-buttons">
+                {!isLoggedIn && (
+                    <>
+                    <button onClick={() => navigate("/login")}>Login</button>
+                    <button onClick={() => navigate("/register")}>Sign Up</button>
+                    </>
+                )}
+                {isLoggedIn && <button onClick={handleLogout}>Logout</button>}
+                </div>
+            </nav>
+            <div className="lab-view-container">
+                <h1 className="lab-view-title">Lab Details</h1>
+                <div className="lab-view-content">
+                    <div><label className="lab-view-label">Name:</label> {labname}</div>
+                    <div><label className="lab-view-label">Location:</label> {labLocation}</div>
+                    <div><label className="lab-view-label">Website:</label> <a className="lab-view-link" href={website} target="_blank" rel="noopener noreferrer">{website}</a></div>
+                    <div><label className="lab-view-label">Field of Study:</label> {field}</div>
+                </div>
             </div>
         </div>
-        
     )
 };
 
