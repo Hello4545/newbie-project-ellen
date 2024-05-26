dc8d9f7cd5210217679d3212deff80e7f01a86de
Hello4545 <sbp3948@gmail.com>
2024-05-26 07:16:47 +0000

    add file

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: 'newbie-project-ellen', // 애플리케이션 이름
-        script: './index.js', // 애플리케이션 진입 파일
+        script: './src/index.js', // 애플리케이션 진입 파일
         instances: 1, // 실행할 인스턴스 수
         autorestart: true, // 자동 재시작 설정
         watch: false, // 파일 변경 감지 여부
